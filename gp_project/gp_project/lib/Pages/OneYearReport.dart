da058ee8cc65224b7b6411dd61593f78c24b3939
AsmaaAmr <45433683+AsmaaAmr@users.noreply.github.com>
2020-07-20 13:05:42 +0200

    ReportUIwithoutDatabase

--- gp_project/gp_project/lib/Pages/OneYearReport.dart
+++ gp_project/gp_project/lib/Pages/OneYearReport.dart
@@ -7,8 +7,7 @@ class oneYearReport extends StatefulWidget {
   final User user;
   @override
   _oneYearReportState createState() => _oneYearReportState();
-    oneYearReport({this.currentUser, this.user});
-
+  oneYearReport({this.currentUser, this.user});
 }
 
 class _oneYearReportState extends State<oneYearReport> {
@@ -16,14 +15,72 @@ class _oneYearReportState extends State<oneYearReport> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text(
+        title: Text(
           'One Year Report',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
       ),
-      body: Text("data"),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 11.0, right: 10.0),
+            child: Text(
+              "This Report include the measurements, meals and moods of this patient according to One year ago",
+              textAlign: TextAlign.justify,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Measuerments",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Moods",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Meals",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- gp_project/gp_project/lib/Pages/ThreeMonReport.dart
+++ gp_project/gp_project/lib/Pages/ThreeMonReport.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gp_project/models/user.dart';
@@ -7,8 +9,7 @@ class ThreeMonthReport extends StatefulWidget {
   final User user;
   @override
   _ThreeMonthReportState createState() => _ThreeMonthReportState();
-    ThreeMonthReport({this.currentUser, this.user});
-
+  ThreeMonthReport({this.currentUser, this.user});
 }
 
 class _ThreeMonthReportState extends State<ThreeMonthReport> {
@@ -16,14 +17,72 @@ class _ThreeMonthReportState extends State<ThreeMonthReport> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text(
+        title: Text(
           '3-Months Report',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
       ),
-      body: Text("data"),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 11.0, right: 10.0),
+            child: Text(
+              "This Report include the measurements, meals and moods of this patient according to 3-Months ago",
+              textAlign: TextAlign.justify,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Measuerments",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+                    Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Moods",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+                    Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              "Meals",
+              textAlign: TextAlign.left,
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
