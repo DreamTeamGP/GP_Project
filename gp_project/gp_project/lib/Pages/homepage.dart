38abd73e3f262d0427ed92cf4b69e24b1441beb0
esraamosllam <49130365+esraamosllam@users.noreply.github.com>
2020-07-21 13:44:35 +0200

    Revert "Report2"
    This reverts commit a0d7ab8becab82e917de55007e85daf09b89c8be.

--- gp_project/gp_project/lib/Pages/homepage.dart
+++ gp_project/gp_project/lib/Pages/homepage.dart
@@ -7,9 +7,6 @@ import 'package:gp_project/Classes/User.dart';
 import 'package:gp_project/Auth/line.dart';
 import 'package:gp_project/Pages/Detailsuser.dart';
 import 'package:gp_project/Pages/MeasurementGraph.dart';
-import 'package:gp_project/Pages/OneYearReport.dart';
-import 'package:gp_project/Pages/SixMonReport.dart';
-import 'package:gp_project/Pages/ThreeMonReport.dart';
 import 'package:gp_project/Pages/profileWidget.dart';
 import 'package:gp_project/Pages/profiledrawer.dart';
 import 'package:gp_project/Pages/measurementPopup.dart';
@@ -178,15 +175,6 @@ class _HomePageState extends State<HomePage> {
                     ),
                     onPressed: () {
                       Navigator.of(context).pop();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ThreeMonthReport(
-                            currentUser: widget.user,
-                          ),
-                        ),
-                      );
-                      // Navigator.of(context).pop();
                     },
                     color: Colors.cyan,
                   ),
@@ -204,15 +192,6 @@ class _HomePageState extends State<HomePage> {
                     ),
                     onPressed: () {
                       Navigator.of(context).pop();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => sixMonthReport(
-                            currentUser: widget.user,
-                          ),
-                        ),
-                      );
-                      // Navigator.of(context).pop();
                     },
                     color: Colors.cyan,
                   ),
@@ -230,15 +209,6 @@ class _HomePageState extends State<HomePage> {
                     ),
                     onPressed: () {
                       Navigator.of(context).pop();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => oneYearReport(
-                            currentUser: widget.user,
-                          ),
-                        ),
-                      );
-                      // Navigator.of(context).pop();
                     },
                     color: Colors.cyan,
                   ),
