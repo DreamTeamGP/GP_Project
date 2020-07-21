6181f23fa740abd3712d109f3bbe3c3f4d0ca695
esraamosllam <49130365+esraamosllam@users.noreply.github.com>
2020-07-21 13:44:30 +0200

    Revert "ReportUIwithoutDatabase"
    This reverts commit da058ee8cc65224b7b6411dd61593f78c24b3939.

--- gp_project/gp_project/lib/Pages/OneYearReport.dart
+++ gp_project/gp_project/lib/Pages/OneYearReport.dart
@@ -7,7 +7,8 @@ class oneYearReport extends StatefulWidget {
   final User user;
   @override
   _oneYearReportState createState() => _oneYearReportState();
-  oneYearReport({this.currentUser, this.user});
+    oneYearReport({this.currentUser, this.user});
+
 }
 
 class _oneYearReportState extends State<oneYearReport> {
@@ -15,72 +16,14 @@ class _oneYearReportState extends State<oneYearReport> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title:  Text(
           'One Year Report',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 15.0, left: 11.0, right: 10.0),
-            child: Text(
-              "This Report include the measurements, meals and moods of this patient according to One year ago",
-              textAlign: TextAlign.justify,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Measuerments",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Moods",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Meals",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: Text("data"),
     );
   }
 }
--- gp_project/gp_project/lib/Pages/SixMonReport.dart
+++ gp_project/gp_project/lib/Pages/SixMonReport.dart
@@ -7,7 +7,8 @@ class sixMonthReport extends StatefulWidget {
   final User user;
   @override
   _sixMonthReportState createState() => _sixMonthReportState();
-  sixMonthReport({this.currentUser, this.user});
+    sixMonthReport({this.currentUser, this.user});
+
 }
 
 class _sixMonthReportState extends State<sixMonthReport> {
@@ -15,72 +16,14 @@ class _sixMonthReportState extends State<sixMonthReport> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title:  Text(
           '6-Months Report',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 15.0, left: 11.0, right: 10.0),
-            child: Text(
-              "This Report include the measurements, meals and moods of this patient according to 6-Months ago",
-              textAlign: TextAlign.justify,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Measuerments",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Moods",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: Text(
-              "Meals",
-              textAlign: TextAlign.left,
-              softWrap: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: Text("data"),
     );
   }
 }
